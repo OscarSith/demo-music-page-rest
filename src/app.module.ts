0301ea21ce8c3484be9dcf9c27a2c7c487199bf7
Oscar Larriega <larriega@gmail.com>
2024-04-12 00:23:03 -0500

    Add static serve files

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,15 @@ import { LibrariesDataModule } from './libraries-data/libraries-data.module';
 import { Library } from './libraries/entities/library.entity';
 import { LibraryData } from './libraries-data/entities/library-data.entity';
 import { Artist } from './artists/entities/artist.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api/(.*)'],
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
