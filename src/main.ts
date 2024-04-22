57a0db80cd4f206148ed30c3f08abcfcdbd7577f
Oscar Larriega <larriega@gmail.com>
2024-04-22 15:48:45 -0500

    Added static assets

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useStaticAssets(join(__dirname, '..', 'public'), {
+    prefix: '/assets',
+  });
 
   await app.listen(3000);
 }
